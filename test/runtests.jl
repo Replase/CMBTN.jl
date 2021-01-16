55ed4ca37ea385e1a389665a258d9f3a678d4d15
Replase <alan.emi10@hotmail.com>
2021-01-16 11:35:58 -0600

    pruebas

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,16 @@
 using Test
+using HTTP
+#a="codigos-postales.csv"
+#f=CMBTN.DFCSV(a,true)  #true porque es un archivo divido por ;
+#size(f)
+#@testset "CMBTN.jl" begin
 
-
-@test true
+#end
+#@test true
+#a=DataFrame(a=[1,2,3,4],b=["agua","pasa","por","mi"],c=[4,3,2,1])
+#CMBTN.filecsv(a)
+a="http://datosabiertos.salud.gob.mx/gobmx/salud/datos_abiertos/datos_abiertos_covid19.zip"
+b="C:\\Users\\Alan\\Documents\\GitHub"
+c=CMBTN.downl(a,b)
+#a=HTTP.download(a,b)
+println("$c")
