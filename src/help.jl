219a9deb6feb74c70fcf385d5f4d075b4c1fbdf3
Replase <alan.emi10@hotmail.com>
2021-01-16 11:35:43 -0600

    Funciones que ayuda

--- src/help.jl
+++ src/help.jl
@@ -5,8 +5,9 @@ using InfoZIP,DataFrames,HTTP,CSVFiles,ZipFile,ExcelFiles,XLSX
 function decompress(zipp::String,dir::String)  #Descomprime un .zip dando su direccion y donde cae
     InfoZIP.unzip(zipp,dir)
 end
-function decompress(zipp::String) #descomprime un .zip y lo guarda el la direccion actual
-    InfoZIP.unzip(zipp,pwd())
+function decompress(zipp::String)::String #descomprime un .zip y lo guarda el la direccion actual
+    a=InfoZIP.unzip(zipp,pwd())
+    return a
 end
 
 
@@ -18,11 +19,28 @@ function DFCSV(csvv::String,g::Bool)::DataFrame
     end
     return f
 end
-
-#function DFXLX(xlx::String,sheet::Int64)::DataFrame
-#    w=XLSX.readxlsx(xlx)
-#    list=XLSX.sheetnames(w)                               intente abrir xlsx pero no pude
-#    XLSX.close(w)
-#    f=DataFrame(XLSX.readtable(xlx,list[sheet]))
-#    return f
-#end
+function filecsv(file::DataFrame)
+    save("file.csv",file)                   #crea un arhchivo csv en el directorio base
+end
+function downl(url::String,dir::String)::String
+        a=HTTP.download(url,dir)                            #Descarga un archivo y si es un zip lo descomprime
+        b=sizeof(a)
+        c=b-2
+        tipo=a[c]*a[c+1]*a[c+2]
+        if tipo == "zip"
+            decompress(a,dir)
+            return ""
+        end
+        return a
+end
+function downl(url::String)::String
+    a=HTTP.download(url,pwd())
+    b=sizeof(a)
+    c=b-2
+    tipo=a[c]*a[c+1]*a[c+2]
+    if tipo == "zip"
+        decompress(a,pwd())
+        return pwd()
+    end
+    return a
+end
