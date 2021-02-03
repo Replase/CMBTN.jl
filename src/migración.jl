9a151e10859e35e69bb5dd6e92393eb9b0b3ffee
beto02001 <69131862+beto02001@users.noreply.github.com>
2021-02-02 22:40:28 -0600

    Update migración.jl

--- src/migración.jl
+++ src/migración.jl
@@ -1,7 +1,9 @@
 using CSVFiles, DataFrames
 #a = df_migracion()
 
-function df_migracion()::DataFrame
-    df = DataFrame(load("indimigra.csv"))
+function df_migracion(path::String)::DataFrame
+    archi = "\\src\\indimigra.csv"
+    archi = path * archi
+    df = DataFrame(load(archi))
     return df
 end
